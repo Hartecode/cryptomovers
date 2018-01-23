820cb1c616d5cb6cec5a26d7ad5eef041742871c
Sean Harte <scsharte@gmail.com>
2018-01-22 23:44:24 -0500

    added the lead coin section to html. worked on the css for the lead coin section.  Added highchart framework cdn to the html page. Updated the index.js with chart for the lead coin section.

--- javascript/index.js
+++ javascript/index.js
@@ -12,7 +12,10 @@ const highToLowPct = [];
 const requestedNumber = 50;
 
 //url for price
-let cryptoComparePriceUrl = "https://min-api.cryptocompare.com/data/pricemultifull?";
+const cryptoComparePriceUrl = "https://min-api.cryptocompare.com/data/pricemultifull?";
+
+//url historical price url
+const histPriceUrl = "https://min-api.cryptocompare.com/data/histoday?";
 
 //this function will get data from the coinlist api
 function getBasicInfo(callback) {
@@ -28,8 +31,8 @@ function addDataToGeneralInfo(data) {
 
 //this fuunction will call the price data for all the coins 
 function getCurrnetPriceData(url, coinlist, callback, numberOfCoins) {
-	const query = url + 'fsyms='+ coinlist.slice(0,numberOfCoins).join(",") + '&tsyms=USD';
-	console.log(query);
+	let query = url + 'fsyms='+ coinlist.slice(0,numberOfCoins).join(",") + '&tsyms=USD';
+	// console.log(query);
 	$.getJSON(query, callback);
 }
 
@@ -40,7 +43,8 @@ function updateData(data) {
 	fromObjectToArray(currerntCoinPriceData, requestedNumber, listOfCoins);
 	loadTopWinThreeCoin (highToLowPct, basicCoinInfo);
 	loadTopLosThreeCoin (highToLowPct, basicCoinInfo);
-	loadLeaderLoserBoard(highToLowPct, basicCoinInfo); 
+	loadLeaderLoserBoard(highToLowPct, basicCoinInfo);
+	topChart(histPriceUrl, 'dayLeaderChart' , highToLowPct); 
 	}
 
 //this funcition take the data from the JSON and puts it in an area which sorts it from heightest percent change to lowest
@@ -97,7 +101,6 @@ function loadTopLosThreeCoin (arr, basicInfo) {
 
 //this function takes two arrguments price data and coin info.  The data from those arrgument will be entered into the html doc
 function boardHtml(priceData, coinInfo) {
-	console.log(coinInfo);
 	return `<li>
 				<img class="top3sym" src="https://www.cryptocompare.com${coinInfo.ImageUrl}" name="${coinInfo.CoinName}">
 				<div class="topContent"><p>${coinInfo.FullName} <span class="priceInc">${priceData.USD.PRICE}</span></p></div>
@@ -108,8 +111,8 @@ function boardHtml(priceData, coinInfo) {
 //this function loads the top 3 coins with % change
 function loadLeaderLoserBoard(arr, basicInfo) {
 	let htmlContent = arr.map(function(priceData) {
-		console.log(priceData.USD.KEY);
-		console.log(arr.indexOf(priceData));
+		// console.log(priceData.USD.KEY);
+		// console.log(arr.indexOf(priceData));
 		return boardHtml(priceData, basicInfo[priceData.USD.KEY]);
 	});
 
@@ -138,6 +141,55 @@ function aboutButton() {
 	});
 }
 
+//this function converts the data from the json to a usable array for chart
+function usableChartData(json) {
+	let fullData = json.Data;
+	let histData = fullData.map(function(obj){
+						return [obj.time, obj.close];
+					});
+	return histData;
+}
+
+
+//this will call data for the lead coin and put it in the main page chart
+
+//this is a chart fucntion which takes a few argument
+function topChart(url, idHTML , arr){
+	let symb = arr[0].USD.KEY;
+	let query = {
+		fsym: symb,
+		tsym: 'USD',
+		allData: true,
+		aggregate: 1
+	}
+	
+
+	$.getJSON(url, query,function(data){
+		
+		console.log(usableChartData(data));
+		Highcharts.stockChart(idHTML, {
+
+
+	        rangeSelector: {
+	            selected: 1
+	        },
+
+	        title: {
+	            text: `${symb} Coin Price`
+	        },
+
+	        series: [{
+	            name: symb,
+	            data: usableChartData(data),
+	            tooltip: {
+	                valueDecimals: 2
+	            }
+	        }]
+	    });
+	});
+
+	
+}
 
 //this function runs the program
 function runApp() {
